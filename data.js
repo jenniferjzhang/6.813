c9dd7adb8b4f1f9904e9184d15c5f04025fd32e2
Henry Tareque <htareque@mit.edu>
2015-04-15 13:45:05 -0400

    meets data file

---
+++ data.js
@@ -0,0 +1,42 @@
+var meets = [
+	{
+		name:"Engineer's Cup",
+		date:"4/4",
+		location:"Home",
+	},
+	{
+		name:"Colby Invitational",
+		date:"4/11",
+		location:"Colby College",
+	},
+	{
+		name:"Sean Collier Invitational",
+		date:"4/18",
+		location:"Home",
+	},
+	{
+		name:"NEWMAC Championship",
+		date:"4/25",
+		location:"Home",
+	},
+	{
+		name:"New England Division III Championship",
+		date:"4/30-5/2",
+		location:"Home",
+	},
+	{
+		name:"NEICAAA",
+		date:"5/7-5/9",
+		location:"Home",
+	},
+	{
+		name:"ECAC Division III Championship",
+		date:"5/14-5/15",
+		location:"Springfield College",
+	},
+	{
+		name:"NCAA Division III Championship",
+		date:"5/21-5/23",
+		location:"St. Lawrence University",
+	},
+]
