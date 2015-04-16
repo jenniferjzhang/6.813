2a81431cb46b0cf89df3eee3244971215ad5d5ef
Bruno Faviero <bfaviero@mit.edu>
2015-04-15 23:43:05 -0400

    meet graph

--- meets.js
+++ meets.js
@@ -82,6 +82,78 @@ $(document).ready(function() {
         $('.dashboard-content-bottom').transition('slide right')
         .removeClass('hidden')
         .addClass('visible');
+
+        var margin = {top: 20, right: 0, bottom: 30, left: 30},
+            width = $('.predicted-results-graph').width() - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "");
+
+        var svg = d3.select(".predicted-results-graph").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          x.domain(["MIT", "BU", "Harvard", "Tufts"]);
+
+          y.domain([0, 18]);
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(0)")
+              .attr("x", 200)
+              .attr("y", 20)	
+              .style("text-anchor", "end")
+              .text("Points");
+
+          var data = [
+        		{letter: 'MIT', frequency: 18, stroke: 'green', color:'rgba(0, 128, 0, 0.2)'},
+              	{letter: 'MIT', frequency: 10, color: 'gray'},
+              	{letter: 'BU', frequency: 12, stroke: 'red', color:'rgba(128, 0, 0, 0.2)'},
+              	{letter: 'BU', frequency: 7, color: 'gray'}, 
+              	{letter: 'Harvard', frequency: 8, stroke: 'red', color:'rgba(128, 0, 0, 0.2)'},
+              	{letter: 'Harvard', frequency: 6, color: 'gray'},
+              	{letter: 'Tufts', frequency: 7, stroke: 'red', color:'rgba(128, 0, 0, 0.2)'},
+              	{letter: 'Tufts', frequency: 4, color: 'gray'}]
+          svg.selectAll(".bar")
+              .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return x(d.letter); })
+              .attr("width", x.rangeBand())
+              .attr("y", function(d) { return y(d.frequency); })
+              .attr("height", function(d) { return height - y(d.frequency); })
+              .attr('fill', function(d) { return d.color})
+              .style("stroke-dasharray", ("2, 2")) 
+              .style("stroke", function(d) { return d.stroke})
+              .style("stroke-width", 3)
+
+
+        function type(d) {
+          d.frequency = +d.frequency;
+          return d;
+        }
     });
 });
 
